396581d6b24b6822ffc34a515625620891e69994
Sachin Charakhwal <sachincharakhwal@gmail.com>
2024-11-10 19:48:28 +0530

    add build workflow

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Iosevka Font";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [ nodejs zip nil ttfautohint ];
+      };
+    });
+}
